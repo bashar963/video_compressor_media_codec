18703967efea98f63faf29826aa27b40d2bdd169
bashar alkaddah <basharqaddah@gmail.com>
2023-10-17 13:52:01 +0300

    initial commit

--- android/src/main/kotlin/com/bashar963/video_compressor_media_codec/VideoCompressorMediaCodecPlugin.kt
+++ android/src/main/kotlin/com/bashar963/video_compressor_media_codec/VideoCompressorMediaCodecPlugin.kt
@@ -6,6 +6,7 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import kotlinx.coroutines.runBlocking
 
