023373f5fe6976ed627d194b366bedf577694a98
BASHAR ALKADDAH <basharqaddah@gmail.com>
2025-11-13 22:56:58 +0300

    Update VideoCompressorMediaCodecPlugin.kt

--- android/src/main/kotlin/com/bashar963/video_compressor_media_codec/VideoCompressorMediaCodecPlugin.kt
+++ android/src/main/kotlin/com/bashar963/video_compressor_media_codec/VideoCompressorMediaCodecPlugin.kt
@@ -1,6 +1,6 @@
 package com.bashar963.video_compressor_media_codec
 
-
+import androidx.annotation.NonNull
 import com.bashar963.video_compressor_media_codec.videocompressor.VideoCompress
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.EventChannel
@@ -9,132 +9,102 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
-
-
 /** VideoCompressorMediaCodecPlugin */
-class VideoCompressorMediaCodecPlugin: FlutterPlugin, MethodCallHandler, EventChannel.StreamHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
-  private lateinit var eventChannel : EventChannel
-  private var mEventSink: EventChannel.EventSink? = null
-  private var videoCompressTask:  VideoCompress.VideoCompressTask? = null
+class VideoCompressorMediaCodecPlugin : FlutterPlugin, MethodCallHandler, EventChannel.StreamHandler {
 
+    private lateinit var channel: MethodChannel
+    private lateinit var eventChannel: EventChannel
+    private var eventSink: EventChannel.EventSink? = null
+    private var videoCompressTask: VideoCompress.VideoCompressTask? = null
 
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "video_compressor_media_codec")
-    channel.setMethodCallHandler(this)
-    eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, "video_compressor_media_codec/compressVideoProgress")
-    eventChannel.setStreamHandler(this)
-  }
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "video_compressor_media_codec")
+        channel.setMethodCallHandler(this)
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
-
-    if (call.method == "compressVideo") {
-
-      val compressType = call.argument<String>("compressType")
-      val filePath = call.argument<String>("filePath")
-      val outputFilePath = call.argument<String>("outputFilePath")
-
-      if(filePath==null || outputFilePath==null){
-            result.error("File Path Error", "File Path Error", "File Path Error")
-      }
-
-
-
-
-      when (compressType) {
-          "high" -> {
-            highCompress(filePath!!,outputFilePath!!, result)
-          }
-          "medium" -> {
-            mediumCompress(filePath!!,outputFilePath!!, result)
-          }
-          "low" -> {
-            lowCompress(filePath!!,outputFilePath!!, result)
-          }
-          else -> {
-              result.error("Compress Type Error", "Compress Type Error", "Compress Type Error")
-          }
-      }
-    } else if(call.method == "cancelVideoCompressTask"){
-        if(videoCompressTask!=null){
-            result.success( videoCompressTask!!.cancel(true))
-        }else{
-            result.success(false)
-        }
-    } else {
-      result.notImplemented()
+        eventChannel = EventChannel(
+            flutterPluginBinding.binaryMessenger,
+            "video_compressor_media_codec/compressVideoProgress"
+        )
+        eventChannel.setStreamHandler(this)
     }
-  }
 
-  private fun highCompress(filePath: String, outputFilePath:String, result: Result) {
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        when (call.method) {
+            "compressVideo" -> {
+                val compressType = call.argument<String>("compressType")
+                val filePath = call.argument<String>("filePath")
+                val outputFilePath = call.argument<String>("outputFilePath")
 
-      videoCompressTask=  VideoCompress.compressVideoHigh(filePath, outputFilePath, object : VideoCompress.CompressListener {
-        override fun onStart() {}
-        override fun onSuccess(compressVideoPath: String) {
-          result.success(compressVideoPath)
+                if (filePath.isNullOrEmpty() || outputFilePath.isNullOrEmpty()) {
+                    result.error("FILE_PATH_ERROR", "File path is null or empty", null)
+                    return
+                }
+
+                when (compressType) {
+                    "high" -> compressVideo(filePath, outputFilePath, result, CompressLevel.HIGH)
+                    "medium" -> compressVideo(filePath, outputFilePath, result, CompressLevel.MEDIUM)
+                    "low" -> compressVideo(filePath, outputFilePath, result, CompressLevel.LOW)
+                    else -> result.error("COMPRESS_TYPE_ERROR", "Invalid compression type", null)
+                }
+            }
+
+            "cancelVideoCompressTask" -> {
+                val canceled = videoCompressTask?.cancel(true) ?: false
+                result.success(canceled)
+            }
+
+            else -> result.notImplemented()
         }
-        override fun onFail() {
-          result.error("Compress Error", "Compress Error", "Compress Error")
+    }
+
+    private fun compressVideo(
+        filePath: String,
+        outputFilePath: String,
+        result: Result,
+        level: CompressLevel
+    ) {
+        val listener = object : VideoCompress.CompressListener {
+            override fun onStart() {}
+
+            override fun onSuccess(compressVideoPath: String) {
+                // Ensure result is returned only once
+                result.success(compressVideoPath)
+                videoCompressTask = null
+            }
+
+            override fun onFail() {
+                result.error("COMPRESS_ERROR", "Video compression failed", null)
+                videoCompressTask = null
+            }
+
+            override fun onProgress(percent: Float) {
+                eventSink?.success(percent)
+            }
         }
-        override fun onProgress(percent: Float) {
-          mEventSink?.success(percent)
+
+        videoCompressTask = when (level) {
+            CompressLevel.HIGH -> VideoCompress.compressVideoHigh(filePath, outputFilePath, listener)
+            CompressLevel.MEDIUM -> VideoCompress.compressVideoMedium(filePath, outputFilePath, listener)
+            CompressLevel.LOW -> VideoCompress.compressVideoLow(filePath, outputFilePath, listener)
         }
-      }
-      )
+    }
 
-  }
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+        eventChannel.setStreamHandler(null)
+        eventSink = null
+        videoCompressTask = null
+    }
 
-  private fun mediumCompress(filePath: String,outputFilePath:String, result: Result) {
+    override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+        eventSink = events
+    }
 
-      videoCompressTask=  VideoCompress.compressVideoMedium(filePath, outputFilePath, object : VideoCompress.CompressListener {
-        override fun onStart() {}
-        override fun onSuccess(compressVideoPath: String) {
-          result.success(compressVideoPath)
-        }
-        override fun onFail() {
-          result.error("Compress Error", "Compress Error", "Compress Error")
-        }
-        override fun onProgress(percent: Float) {
+    override fun onCancel(arguments: Any?) {
+        eventSink = null
+    }
 
-          mEventSink?.success(percent)
-        }
-      }
-      )
-
-  }
-
-  private fun lowCompress(filePath: String,outputFilePath:String, result: Result) {
-
-      videoCompressTask= VideoCompress.compressVideoLow(filePath, outputFilePath, object : VideoCompress.CompressListener {
-        override fun onStart() {}
-        override fun onSuccess(compressVideoPath: String) {
-          result.success(compressVideoPath)
-        }
-        override fun onFail() {
-          result.error("Compress Error", "Compress Error", "Compress Error")
-        }
-        override fun onProgress(percent: Float) {
-          mEventSink?.success(percent)
-        }
-      }
-      )
-
-  }
-
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-    eventChannel.setStreamHandler(null)
-  }
-
-  override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
-    mEventSink = events
-  }
-
-  override fun onCancel(arguments: Any?) {
-    mEventSink = null
-  }
+    private enum class CompressLevel {
+        HIGH, MEDIUM, LOW
+    }
 }
